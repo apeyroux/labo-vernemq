38bc0b425aa44b5960a6e2cc063305b563cb1ee5
Alexandre Peyroux <alex@px.io>
2019-05-21 19:03:17 +0200

    test simple webhookplugins

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,47 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
+import GHC.Generics
 import Web.Scotty
+import Data.Aeson
+import Data.Text.Lazy as TL
 
 import Data.Monoid (mconcat)
 
+-- {\"username\":null,\"mountpoint\":\"\",\"client_id\":\"mosqpub|6381-xps15.px.i\",\"qos\":0,\"topic\":\"inventories/test\",\"payload\":\"dGVzdA==\",\"retain\":false}
+data OnPublish = OnPublish {
+  opUserName :: Maybe String
+  , opMountPoint :: String
+  , opClientID :: String
+  , opQOS :: Integer
+  , opTopic :: String
+  , opPayload :: String
+  , opRetain :: Bool
+  } deriving (Show, Generic)
+
+instance FromJSON OnPublish where
+  parseJSON = withObject "OnPublish" $ \v -> OnPublish
+        <$> v .:? "username"
+        <*> v .: "mountpoint"
+        <*> v .: "client_id"
+        <*> v .: "qos"
+        <*> v .: "topic"
+        <*> v .: "payload"
+        <*> v .: "retain"
+
+-- Just "on_publish"
 -- https://docs.vernemq.com/plugindevelopment/webhookplugins
 main = scotty 3000 $
   post "/inventories" $ do
-    b <- body
     h <- header "vernemq-hook"
-    liftAndCatchIO $ print b
-    liftAndCatchIO $ print h
-    text "ok"
+    case h of
+      Just "on_publish" -> do
+        b <- body
+        case decode b :: Maybe OnPublish of
+          Just op -> do
+            liftAndCatchIO $ print op
+            text $ TL.pack $ opPayload op
+          Nothing -> do
+            liftAndCatchIO $ print b
+            text "cant decode body"
+      _ -> text "not publish event" --
